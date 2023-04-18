dc877f1d92224a80603d81d224ed5c2815891bf9
Ra1n <theraingame@mail.ru>
2023-04-18 18:09:07 +0300

    react 17

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import App from './components/app/app';
 import './index.css';
 
-import { createRoot } from 'react-dom/client';
+// React 18 -----------------------------------
+// import { createRoot } from 'react-dom/client';
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-root.render(<App />);
+// const domNode = document.getElementById('root');
+// const root = createRoot(domNode);
+// root.render(<App />);
 
 // React 17 -----------------------------------
-// import ReactDOM from 'react-dom';
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
