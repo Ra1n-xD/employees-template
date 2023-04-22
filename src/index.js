15b279fb68c44b727fed6bd6b187562a0cbf27af
Ra1n <theraingame@mail.ru>
2023-04-22 14:51:58 +0300

    Обновление до React 18

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import App from './components/app/app';
 import './index.css';
 
 // React 18 -----------------------------------
-// import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-// const domNode = document.getElementById('root');
-// const root = createRoot(domNode);
-// root.render(<App />);
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);
 
 // React 17 -----------------------------------
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
